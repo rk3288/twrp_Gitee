e4656967ab5e7dc501f8051542ae48433a0cdcef
rtyutechstudio <rtyutechstudio@icloud.com>
2026-05-10 11:49:41 +0800

    ..few..f

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo twrp_T1030M-eng
+add_lunch_combo twrp_T1030M-userdebug
+add_lunch_combo twrp_T1030M-user
