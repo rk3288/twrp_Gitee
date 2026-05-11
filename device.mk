5a87fb09e9ffa91b997156b158978a60f48acd23
rtyutechstudio <rtyutechstudio@icloud.com>
2026-05-11 23:04:10 +0800

    nwejlnfcew cmweklncwecmlw mkwcjs

--- device.mk
+++ device.mk
@@ -40,8 +40,9 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
 
 # Health Hal
 PRODUCT_PACKAGES += \
@@ -62,5 +63,12 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
     
 
